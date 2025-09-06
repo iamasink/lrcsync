1330d9b7a3ad1a6c497a6c66ebdff4d0b8949882
iamasink <iamasink@proton.me>
2025-09-06 10:29:04 +0100

    feat: add formattimestamp

--- src/lib/parseLRC.ts
+++ src/lib/parseLRC.ts
@@ -179,23 +179,28 @@ export function exportWithMetadata(lines: LyricLine[]) {
 /**
  * Formats a time value into MM:SS.SS format.
  *
- * @param t - Time value in milliseconds.
+ * @param time - Time value in milliseconds.
  * @returns Formatted time string (MM:SS.SS).
  */
-export function formatTime(t: number) {
-	if (t == -1) return ``
+export function formatTime(time: number) {
+	if (time == -1) return ``
 
-	if (!Number.isFinite(t) || t < 0) t = 0
-	const m = Math.floor(t / 60000)
+	if (!Number.isFinite(time) || time < 0) time = 0
+	const m = Math.floor(time / 60000)
 		.toString()
 		.padStart(2, "0")
-	const s = Math.floor((t % 60000) / 1000)
+	const s = Math.floor((time % 60000) / 1000)
 		.toString()
 		.padStart(2, "0")
-	const cs = Math.floor((t % 1000) / 10) // centiseconds
+	const cs = Math.floor((time % 1000) / 10) // centiseconds
 		.toString()
 		.padStart(2, "0")
 	return `${m}:${s}.${cs}`
+}
+
+export function formatTimestamp(time: number) {
+	if (time == -1) return ``
+	return `[${formatTime(time)}]`
 }
 
 
