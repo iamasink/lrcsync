c10b3011a9aa3fe6e154a045e0941cbc2ea72aa9
iamasink <iamasink@proton.me>
2025-09-25 06:22:39 +0100

    update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,13 +12,18 @@ let { children } = $props()
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
 
 	<title>LRCSync</title>
-	<meta name="description" content="Edit, sync, and preview LRC lyric files easily." />
-	<meta property="og:title" content="LRCSync - LRC Editor & Sync Tool" />
-	<meta property="og:description" content="Edit, sync, and preview LRC lyric files easily." />
+	<meta property="og:title" content="LRCSync - Advanced LRC File Editor tool" />
+	<meta name="description" content="Edit, sync, and preview LRC lyric files." />
+	<meta property="og:description" content="Edit, sync, and preview LRC lyric files." />
 	<!-- <meta property="og:image" content="/static/preview.png" /> -->
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://lrcsync.vercel.app" />
-	<!-- <meta name="twitter:card" content="summary_large_image" /> -->
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="LRCSync - Advanced LRC File Editor tool" />
+	<meta name="twitter:description" content="Edit, sync, and preview LRC lyric files." />
+	<meta name="twitter:image" content="" />
 </svelte:head>
 
 {@render children?.()}
