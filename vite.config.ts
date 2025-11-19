e8c8fe9bda4e5f752a65b21894f8722280f51ca5
iamasink <iamasink@proton.me>
2025-11-19 07:23:56 +0000

    add chrome devtools thingie

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,26 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { execSync } from 'child_process'
-import { defineConfig } from 'vite'
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { execSync } from 'child_process';
+import { defineConfig } from 'vite';
 
 function getGitInfo() {
 	try {
-		const hash = execSync('git rev-parse HEAD').toString().trim()
-		const branch = execSync('git symbolic-ref --short HEAD').toString().trim()
-		return { hash, branch }
+		const hash = execSync('git rev-parse HEAD').toString().trim();
+		const branch = execSync('git symbolic-ref --short HEAD').toString().trim();
+
+		return { hash, branch };
 	} catch {
-		return { hash: undefined, branch: undefined }
+		return { hash: undefined, branch: undefined };
 	}
 }
 
-const { hash, branch } = getGitInfo()
+const { hash, branch } = getGitInfo();
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), devtoolsJson()],
 	build: { minify: false },
 	define: {
 		__GIT_HASH__: JSON.stringify(hash),
-		__GIT_BRANCH__: JSON.stringify(branch),
+		__GIT_BRANCH__: JSON.stringify(branch)
 	}
-})
+});
