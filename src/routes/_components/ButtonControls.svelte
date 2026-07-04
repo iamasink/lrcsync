925c9af33bd32b4c649bf2eb0910a1bbd0846a66
sink <iamasink@proton.me>
2026-07-04 23:37:02 +0100

    add beat synced adjustment and ,. 1ms audio seek

--- src/routes/_components/ButtonControls.svelte
+++ src/routes/_components/ButtonControls.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+import { getBeatFromCurrentTime, getBeatFromTime, getTimeAtBeat } from "$lib/bpm"
 import Button from "$lib/components/Button.svelte"
 import KeybindButton from "$lib/components/KeybindButton.svelte"
 import { historyManager } from "$lib/history.svelte"
-import { cleanup, sortLines, stripBadStuff } from "$lib/parseLRC"
+import { cleanup, roundTimestamp, sortLines, stripBadStuff } from "$lib/parseLRC"
 import { scrollLineIntoView } from "$lib/scroll"
 import { s } from "$lib/state.svelte"
 import { clamp } from "$lib/utils"
 
-let multiplier = $derived(1 * (s.modkeysHeld.shift ? 5 : 1) * (s.modkeysHeld.ctrl ? 10 : 1))
-let stepbuttonvalue = $derived(0.01 * multiplier)
+const shiftMulti = 1 / 10
+const ctrlMulti = 10
+
+let multiplier = $derived(1 * (s.modkeysHeld.shift ? shiftMulti : 1) * (s.modkeysHeld.ctrl ? ctrlMulti : 1))
+let stepbuttonvalue = $derived(0.1 * multiplier)
 let fastforwardbuttonvalue = $derived(1 * multiplier)
+let beatstepbuttonvalue = $derived(1 * (s.modkeysHeld.shift ? 4 : 1))
 
 function handleNextButtonClick() {
 	// try to move forward until a valid line is found
@@ -24,7 +29,7 @@ function handleNextButtonClick() {
 		const time = s.lyrics[i].time
 		s.waveformRef?.seekToTime(time / 1000)
 		scrollLineIntoView(i)
-		s.waveformRef?.updateSelectedRegions([lastLine,currLine])
+		s.waveformRef?.updateSelectedRegions([lastLine, currLine])
 		s.currentAudioLine = i
 	}
 }
@@ -39,10 +44,10 @@ function handlePrevButtonClick() {
 	}
 
 	if (i >= 0) {
-		const time = s.lyrics[i].time + 0.01
+		const time = s.lyrics[i].time
 		s.waveformRef?.seekToTime(time / 1000)
 		scrollLineIntoView(i)
-		s.waveformRef?.updateSelectedRegions([lastLine,currLine])
+		s.waveformRef?.updateSelectedRegions([lastLine, currLine])
 		s.currentAudioLine = i
 	}
 }
@@ -70,6 +75,73 @@ function handleAdjustClick(offsetSec: number, event: MouseEvent) {
 	adjustSelectedLine(adjustmentSec)
 
 	historyManager.pushDebounced(`adjusted line ${s.currentAudioLine}`, { offset: offsetSec })
+}
+
+function handleAdjustClickBeat(offsetBeats: number, beatDivision: number = 1) {
+	const line = s.lyrics[s.currentAudioLine]
+	const lineIndex = s.currentAudioLine
+
+	if (!line || line.time === -1) {
+		console.warn("No valid line selected")
+		return
+	}
+
+	console.log("beatDivision = ", beatDivision, "offsetBeats = ", offsetBeats)
+	if (!s.useBPM) {
+		console.warn("BPM not set, cannot adjust by beat")
+		return
+	}
+	const bpm = s.audioBPM || 60
+	const beatIntervalMs = (60 * 1000) / bpm / beatDivision
+	const lineTimeMs = line.time
+	if (lineTimeMs === -1) {
+		console.warn("Current line has no timestamp, cannot adjust by beat")
+		return
+	}
+
+	// if not close to a beat, snap to the closest next/prev beat, otherwise snap to the next/prev
+	const lineBeat = getBeatFromTime(lineTimeMs) * beatDivision
+	console.log("lineTimeMs = ", lineTimeMs, "ms, lineBeat = ", lineBeat, "beats, offsetBeats = ", offsetBeats, "beats, beatIntervalMs = ", beatIntervalMs, "ms")
+	console.log("timeatbeat ", getTimeAtBeat(lineBeat), "ms")
+
+	if (offsetBeats === 0) return
+	// if were on a beat
+	if (Math.abs(lineBeat - Math.round(lineBeat)) < 0.01) {
+		// move to the next/prev beat
+		const newLineBeat = Math.round(lineBeat) + offsetBeats
+		const newTime = Math.round(getTimeAtBeat(newLineBeat / beatDivision))
+		adjustSelectedLine((newTime - lineTimeMs) / 1000)
+		historyManager.pushDebounced(`adjusted line ${s.currentAudioLine} by ${offsetBeats} beats`, { offset: (newTime - lineTimeMs) / 1000 })
+	} else {
+		// snap to the closest next/prev beat
+		if (offsetBeats > 0) {
+			const nextBeat = Math.ceil(lineBeat)
+			const newTime = Math.round(getTimeAtBeat(nextBeat / beatDivision))
+			// adjustSelectedLine((newTime - lineTimeMs) / 1000)
+			adjustLineSpecific(lineIndex, newTime)
+			historyManager.pushDebounced(`adjusted line ${s.currentAudioLine} to next beat`, { offset: (newTime - lineTimeMs) / 1000 })
+		} else {
+			const prevBeat = Math.floor(lineBeat)
+			const newTime = Math.round(getTimeAtBeat(prevBeat / beatDivision))
+			// adjustSelectedLine((newTime - lineTimeMs) / 1000)
+			adjustLineSpecific(lineIndex, newTime)
+			historyManager.pushDebounced(`adjusted line ${s.currentAudioLine} to previous beat`, { offset: (newTime - lineTimeMs) / 1000 })
+		}
+	}
+}
+
+function adjustLineSpecific(lineIndex: number, timeMs: number) {
+	if (lineIndex < 0 || lineIndex >= s.lyrics.length) {
+		console.warn("invalid line index")
+		return
+	}
+
+	s.lyrics[lineIndex].time = timeMs
+	if (s.waveformRef) {
+		s.waveformRef.seekToTime(timeMs / 1000)
+		s.waveformRef.updateSelectedRegions([lineIndex])
+	}
+	console.log(`adjusted line ${lineIndex} to ${timeMs}ms`)
 }
 
 function adjustSelectedLine(offsetSec: number) {
@@ -110,18 +182,10 @@ function adjustSelectedLine(offsetSec: number) {
 	const newTime = clamp(
 		targetLine.time + (offsetSec * 1000),
 		prevTime + minLineTime,
-		nextTime,
+		nextTime - minLineTime,
 	)
 
-	targetLine.time = newTime
-
-	if (s.waveformRef) {
-		s.waveformRef.seekToTime(newTime / 1000)
-		s.waveformRef.updateSelectedRegions([s.currentAudioLine])
-		// if (s.waveformRef.isPlaying()) {
-		// 	s.waveformRef.play()
-		// }
-	}
+	adjustLineSpecific(targetLineIndex, newTime)
 }
 
 function togglePlayPause() {
@@ -171,6 +235,25 @@ function togglePlayPause() {
 
 		<KeybindButton title={"Move the audio to the start of next line"} onclick={handleNextButtonClick} shortcut={{ key: "down" }}>next line</KeybindButton>
 		<KeybindButton title={"Move the audio to the start of previous line"} onclick={handlePrevButtonClick} shortcut={{ key: "up" }}>prev line</KeybindButton>
+
+		<KeybindButton
+			onclick={() => s.waveformRef?.seekToTime((s.audioTimeMs / 1000) - 0.001)}
+			disabled={s.currentCaretLine < 0}
+			title={`Move back by 1ms`}
+			shortcut={{ key: "," }}
+			ignoremods={true}
+		>
+			,
+		</KeybindButton>
+		<KeybindButton
+			onclick={() => s.waveformRef?.seekToTime((s.audioTimeMs / 1000) + 0.001)}
+			disabled={s.currentCaretLine < 0}
+			title={`Move forward by 1ms`}
+			shortcut={{ key: "." }}
+			ignoremods={true}
+		>
+			.
+		</KeybindButton>
 	</div>
 	<div>
 		<KeybindButton
@@ -191,6 +274,25 @@ function togglePlayPause() {
 			ignoremods={true}
 		>
 			+{stepbuttonvalue.toFixed(2)}s
+		</KeybindButton>
+		<KeybindButton
+			onclick={(e) => handleAdjustClickBeat(-1, beatstepbuttonvalue)}
+			disabled={s.currentCaretLine < 0}
+			title={`Move currently playing line earlier by -1/${beatstepbuttonvalue}`}
+			shortcut={{ key: "1" }}
+			ignoremods={true}
+		>
+			-{beatstepbuttonvalue}
+		</KeybindButton>
+
+		<KeybindButton
+			onclick={(e) => handleAdjustClickBeat(1, beatstepbuttonvalue)}
+			disabled={s.currentCaretLine < 0}
+			title={`Move currently playing line later by 1/${beatstepbuttonvalue} `}
+			shortcut={{ key: "2" }}
+			ignoremods={true}
+		>
+			+{beatstepbuttonvalue}
 		</KeybindButton>
 
 		<!--  -->
