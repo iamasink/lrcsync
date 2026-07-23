fe6f2d5bb0b869cdefcc24ff16c314d067a635b0
sink <iamasink@proton.me>
2026-07-23 06:40:29 +0100

    remove

--- packages/kuroshiro-browser/lib/main.js
+++
@@ -1,5 +0,0 @@
-import { default as Kuroshiro } from "../src/kuroshiro/core.js";
-import { default as KuroshiroAnalyzerKuromoji } from "../src/kuroshiro-analyzer-kuromoji/analyzer.js";
-import { default as Kuromoji } from "../src/kuromoji/kuromoji.js";
-
-export { Kuroshiro, KuroshiroAnalyzerKuromoji, Kuromoji};
